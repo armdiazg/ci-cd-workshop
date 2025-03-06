29c0bbedc7a69558628e73966adec3c6bffa404a
armdiazg <arm.diaz.amazon@gmail.com>
2025-03-06 00:13:33 +0000

    Added Docker build and push

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,12 +1,18 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_codeconnections as codeconnections } from 'aws-cdk-lib';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository;
+}
 
 export class PipelineCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props: ConsumerProps) {
     super(scope, id, props);
 
     const SourceConnection = new codeconnections.CfnConnection(this, 'CICD_Workshop_Connection', {
@@ -21,30 +27,70 @@ export class PipelineCdkStack extends Stack {
       executionMode: codepipeline.ExecutionMode.QUEUED,
     });
 
-    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
-        privileged: true,
-        computeType: codebuild.ComputeType.LARGE,
-      },
-      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
-    });
+    const codeBuild = new codebuild.PipelineProject(
+      this,
+      'CodeBuild',
+      {
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+          privileged: true,
+          computeType: codebuild.ComputeType.LARGE,
+        },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
+      }
+    );
+
+    const dockerBuild = new codebuild.PipelineProject(this, 'DockerBuild', {
+        environmentVariables: {
+          IMAGE_TAG: { value: 'latest' },
+          IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
+          AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+        },
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+          privileged: true,
+          computeType: codebuild.ComputeType.LARGE,
+        },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+      });
+  
+      const dockerBuildRolePolicy = new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ['*'],
+        actions: [
+          'ecr:GetAuthorizationToken',
+          'ecr:BatchCheckLayerAvailability',
+          'ecr:GetDownloadUrlForLayer',
+          'ecr:GetRepositoryPolicy',
+          'ecr:DescribeRepositories',
+          'ecr:ListImages',
+          'ecr:DescribeImages',
+          'ecr:BatchGetImage',
+          'ecr:InitiateLayerUpload',
+          'ecr:UploadLayerPart',
+          'ecr:CompleteLayerUpload',
+          'ecr:PutImage',
+        ],
+      });
+  
+      dockerBuild.addToRolePolicy(dockerBuildRolePolicy);
 
     const sourceOutput = new codepipeline.Artifact();
     const unitTestOutput = new codepipeline.Artifact();
+    const dockerBuildOutput = new codepipeline.Artifact();
 
     pipeline.addStage({
-        stageName: 'Source',
-        actions: [
-          new codepipeline_actions.CodeStarConnectionsSourceAction({
-            actionName: 'GitHub',
-            owner: 'armdiazg',
-            repo: 'ci-cd-workshop',
-            output: sourceOutput,
-            branch: 'main',
-            connectionArn: 'arn:aws:codeconnections:us-east-2:768795771629:connection/6eb9cd09-61eb-4f39-8f47-5802b2da3da8',
-          }),
-        ],
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.CodeStarConnectionsSourceAction({
+          actionName: 'GitHub',
+          owner: 'armdiazg',
+          repo: 'CICD_Workshop',
+          output: sourceOutput,
+          branch: 'main',
+          connectionArn: 'arn:aws:codeconnections:us-east-2:768795771629:connection/6eb9cd09-61eb-4f39-8f47-5802b2da3da8',
+        }),
+      ],
     });
 
     pipeline.addStage({
@@ -58,6 +104,18 @@ export class PipelineCdkStack extends Stack {
         }),
       ],
     });
+
+    pipeline.addStage({
+        stageName: 'Docker-Push-ECR',
+        actions: [
+          new codepipeline_actions.CodeBuildAction({
+            actionName: 'Docker-Build',
+            project: dockerBuild,
+            input: sourceOutput,
+            outputs: [dockerBuildOutput],
+          }),
+        ],
+      });
 
     new CfnOutput(this, 'SourceConnectionArn', {
         value: SourceConnection.attrConnectionArn,
