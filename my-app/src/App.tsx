fc4067f516a201e25b3fa8c9c5b565dacdb66ee3
armdiazg <arm.diaz.amazon@gmail.com>
2025-03-06 00:16:33 +0000

    Added Docker build and push

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>PULL REQUEST TEST 2</p>
+      <p>PULL REQUEST TEST</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
